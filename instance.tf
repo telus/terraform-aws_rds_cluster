127cd14605dcfeabd111c533410425c84cd6b990
Reetu Mutti <reetu@mutti.io>
2018-11-21 11:58:04 -0800

    feat: add final_snapshot_name

--- instance.tf
+++ instance.tf
@@ -14,6 +14,7 @@ resource "aws_db_instance" "default" {
   backup_window = "${var.backup_window}"
   backup_retention_period  = "${var.backup_retention_period}"
   storage_encrypted = "${var.storage_encrypted}"
+  final_snapshot_identifier = "${var.name}_final_snapshot"
 
   lifecycle {
      ignore_changes = ["password"]
